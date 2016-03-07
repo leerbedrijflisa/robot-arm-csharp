6117f9126d8bf0b616e5d3fb84ec69ff548e869d
Bryan Stekelenburg <xerxesmeow@gmail.com>
2016-03-07 14:36:46 +0100

    Initial commit, includes Connecting, Receiving messages from server, move left and move right.

---
+++ Robot-Arm-CSharp/CSharp/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+public class CSharp
+{
+    public float Speed = 0.5f;
+    public int Timeout = 60000; // Timeout in milliseconds
+    public TcpClient tcpclnt;
+    public Stream strm;
+
+    public CSharp()
+    {
+        try
+        {
+            tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting");
+            
+            tcpclnt.Connect("127.0.0.1", 9876);
+
+            strm = tcpclnt.GetStream();
+            ReceiveMessage();
+        }
+
+        catch(Exception e)
+        {
+            Console.WriteLine("Error: " + e.StackTrace);
+        }
+    }
+
+    public void MoveLeft()
+    {
+        SendMessage("Move left");
+    }
+
+    public void MoveRight()
+    {
+        SendMessage("Move right");
+    }
+
+    public void SendMessage(string cmd)
+    {
+        try
+        {
+            cmd = cmd + "\r\n";
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(cmd);
+
+            strm.Write(ba, 0, ba.Length);
+            ReceiveMessage();
+                
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.StackTrace);
+        }
+    }
+
+    public void ReceiveMessage()
+    {
+        byte[] bb = new byte[100];
+        int k = strm.Read(bb, 0, 100);
+        string result = "";
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(Convert.ToChar(bb[i]).ToString());
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Robot-Arm-CSharp/TCPTest/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+
+
+public class TCPTest
+{
+
+    public static void Main()
+    {
+        Connect();
+    }
+
+    public static void Connect()
+    {
+        try
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting");
+            
+            tcpclnt.Connect("127.0.0.1", 9876);
+
+            Stream strm = tcpclnt.GetStream();
+            ReceiveMessage(tcpclnt, strm);
+        }
+
+        catch(Exception e)
+        {
+            Console.WriteLine("Error: " + e.StackTrace);
+        }
+    }
+
+    public static void SendMessage(TcpClient tcpclnt, Stream strm)
+    {
+        try
+        {
+            string cmd = Console.ReadLine();
+            cmd = cmd + "\r\n";
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(cmd);
+
+            strm.Write(ba, 0, ba.Length);
+            ReceiveMessage(tcpclnt, strm);
+                
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.StackTrace);
+        }
+    }
+
+    public static void ReceiveMessage(TcpClient tcpclnt, Stream strm)
+    {
+        byte[] bb = new byte[100];
+        int k = strm.Read(bb, 0, 100);
+        for (int i = 0; i < k; i++)
+            Console.Write(Convert.ToChar(bb[i]));
+        SendMessage(tcpclnt, strm);
+    }
+}
